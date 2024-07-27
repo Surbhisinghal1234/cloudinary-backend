28188bfdb8d0db51f9c4ab966f158bd55da6b156
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-07-27 22:22:14 +0530

    update

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ import 'dotenv/config';
 const PORT = 3000;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    headers: ['Content-Type', 'Authorization']
+  }));
 app.use(express.json());
 
 const username = process.env.MONGO_USERNAME;
